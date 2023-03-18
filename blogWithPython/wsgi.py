9847278b8a62a1c9a27e141870b5970024368222
Benrhu <benrhu@gmail.com>
2023-03-18 15:10:25 +0100

    Wsgi modified for Gunicorn

--- blogWithPython/wsgi.py
+++ blogWithPython/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for blogWithPython project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from gunicorn.app.wsgiapp import run
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogWithPython.settings')
 
 application = get_wsgi_application()
+
+activate_this = '/var/www/grouzy.com/html/venv/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
